cf64e1ab5fbcc1d9603666e2bcc968945726ddbd
KhawajaAhmed <khawajaahmed_2025@depauw.edu>
2024-07-01 01:15:42 -0400

    Added Social Media trend code

--- src/social_media_trends.py
+++ src/social_media_trends.py
@@ -1,40 +1,40 @@
-import pandas as pd
-import tweepy
-from textblob import TextBlob
-from .data_collector import DataCollector
-import logging
+# import pandas as pd
+# import tweepy
+# from textblob import TextBlob
+# from .data_collector import DataCollector
+# import logging
 
-class SocialMediaTrendsCollector(DataCollector):
-    def __init__(self):
-        super().__init__('social_media_trends')
-        self.api = self.authenticate_twitter_app()
+# class SocialMediaTrendsCollector(DataCollector):
+#     def __init__(self):
+#         super().__init__('social_media_trends')
+#         self.api = self.authenticate_twitter_app()
     
-    def authenticate_twitter_app(self):
-        auth = tweepy.OAuth1UserHandler(
-            self.source_config['api_key'],
-            self.source_config['api_secret_key'],
-            self.source_config['access_token'],
-            self.source_config['access_token_secret']
-        )
-        return tweepy.API(auth)
+#     def authenticate_twitter_app(self):
+#         auth = tweepy.OAuth1UserHandler(
+#             self.source_config['api_key'],
+#             self.source_config['api_secret_key'],
+#             self.source_config['access_token'],
+#             self.source_config['access_token_secret']
+#         )
+#         return tweepy.API(auth)
     
-    def fetch_data(self):
-        query = self.source_config['query']
-        max_tweets = self.source_config.get('max_tweets', 100)
-        tweets = tweepy.Cursor(self.api.search_tweets, q=query, lang='en').items(max_tweets)
+#     def fetch_data(self):
+#         query = self.source_config['query']
+#         max_tweets = self.source_config.get('max_tweets', 100)
+#         tweets = tweepy.Cursor(self.api.search_tweets, q=query, lang='en').items(max_tweets)
         
-        tweet_data = []
-        for tweet in tweets:
-            sentiment = TextBlob(tweet.text).sentiment
-            tweet_data.append({
-                'tweet_id': tweet.id_str,
-                'text': tweet.text,
-                'created_at': tweet.created_at,
-                'retweet_count': tweet.retweet_count,
-                'favorite_count': tweet.favorite_count,
-                'polarity': sentiment.polarity,
-                'subjectivity': sentiment.subjectivity
-            })
+#         tweet_data = []
+#         for tweet in tweets:
+#             sentiment = TextBlob(tweet.text).sentiment
+#             tweet_data.append({
+#                 'tweet_id': tweet.id_str,
+#                 'text': tweet.text,
+#                 'created_at': tweet.created_at,
+#                 'retweet_count': tweet.retweet_count,
+#                 'favorite_count': tweet.favorite_count,
+#                 'polarity': sentiment.polarity,
+#                 'subjectivity': sentiment.subjectivity
+#             })
         
-        self.data = pd.DataFrame(tweet_data)
-        logging.info("Social media trends data fetched and analyzed successfully")
+#         self.data = pd.DataFrame(tweet_data)
+#         logging.info("Social media trends data fetched and analyzed successfully")
