1b778af7ea53224121e4cea956e87ba3c4847df9
KhawajaAhmed <khawajaahmed_2025@depauw.edu>
2024-07-01 02:42:27 -0400

    Added logging and error handling

--- src/crop_yield_data.py
+++ src/crop_yield_data.py
@@ -5,14 +5,20 @@ import faostat
 
 class CropYieldDataCollector(DataCollector):
     def __init__(self):
+        logging.info("Initializing CropYieldDataCollector")
         super().__init__('crop_yield_data')
 
     def fetch_data(self):
-
-        mypars = {
-            'area': self.source_config['usa'],
-            'element': self.source_config['yield'],
-            'item': self.source_config['crops']
-            }
-        
-        self.data = faostat.get_data_df('QCL', pars=mypars, strval=False)
+        try:
+            logging.info("Fetching crop yield data")
+            mypars = {
+                'area': self.source_config['usa'],
+                'element': self.source_config['yield'],
+                'item': self.source_config['crops']
+                }
+            
+            self.data = faostat.get_data_df('QCL', pars=mypars, strval=False)
+            
+        except Exception as e:
+            logging.error(f"An error occurred while fetching data: {e}")
+    
