b08e3b4a98eae7be9d21b99d4f6f0f2495051ecf
KhawajaAhmed <khawajaahmed_2025@depauw.edu>
2024-07-01 02:43:43 -0400

    Added parameters from config file & added logging

--- src/industry_sales_trends.py
+++ src/industry_sales_trends.py
@@ -8,6 +8,7 @@ from json import JSONDecodeError
 
 class IndustrySalesTrendsCollector(DataCollector):
     def __init__(self):
+        logging.info("Initializing IndustrySalesTrendsCollector")
         super().__init__('industry_sales_trends')
 
     def fetch_data(self):
@@ -15,22 +16,29 @@ class IndustrySalesTrendsCollector(DataCollector):
         api_key = self.source_config['api_key']
         params = {
             'key': 'api_key',
-            'commodity_desc': 'CORN',
-            'year__GE' : '2012',
-            'state_alpha' : 'VA',
-            'format': 'JSON'
+            'source_desc' : self.source_config['source_desc'],
+            'sector_desc' : self.source_config['sector_desc'],
+            'group_desc' : self.source_config['group_desc'],
+            'commodity_desc' : self.source_config['commodity_desc'],
+            'statisticcat_desc' : self.source_config['statisticcat_desc'],
+            'short_desc' : self.source_config['short_desc'],
+            'domain_desc' : self.source_config['domain_desc'],
+            'agg_level_desc' : self.source_config['agg_level_desc'],
+            'format': self.source_config['format']
         }
         
-        response = fetch_data(base_url, params)
-        
-        if response is not None:
-            try:
-                json_data = response.json()
-                if 'data' in json_data:
-                    self.data = pd.DataFrame(json_data['data'])
-                else:
-                    logging.error("JSON response does not contain 'data' key")
-            except JSONDecodeError as e:
-                logging.error(f"Error decoding JSON response: {e}")
-            except ValueError as e:
-                logging.error(f"Error processing JSON data: {e}")
+        try:
+            response = fetch_data(base_url, params)
+            if response is not None:
+                try:
+                    json_data = response.json()
+                    if 'data' in json_data:
+                        self.data = pd.DataFrame(json_data['data'])
+                    else:
+                        logging.error("JSON response does not contain 'data' key")
+                except JSONDecodeError as e:
+                    logging.error(f"Error decoding JSON response: {e}")
+                except ValueError as e:
+                    logging.error(f"Error processing JSON data: {e}")
+        except requests.RequestException as e:
+            logging.error(f"Error fetching data: {e}")
